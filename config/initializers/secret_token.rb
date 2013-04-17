f02d3178e61338abeb370aed6884628678db5bc4
Rodney Bryant <nzbryant@yahoo.com>
2013-04-17 14:33:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Speedlearner::Application.config.secret_token = '4c645d0651c8a1f2f9c530580fda2f686127212a6ed157ff0e7dd517860d7a645be7cd3d7af8e4c53475eef09d889f605c33ee2690eef50d236ef36cb782e7fb'
